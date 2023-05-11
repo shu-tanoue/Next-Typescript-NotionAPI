469e0a13cff826a22565b4db7daec3cc5e1318d3
Shu <stbook.syu@icloud.com>
2023-05-11 14:30:38 +0900

    Rename

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 const Navbar = () => {
   return (
     <nav className="container mx-auto lg:px-2 px-5 lg:w-2/5">
       <div className="container flex items-center justify-between mx-auto">
         <Link href="/" className="text-2xl font-medium">
-          ShinCode
+          Shu/Blog
         </Link>
         <div>
           <ul className="flex items-center text-sm py-4">
@@ -23,7 +23,7 @@ const Navbar = () => {
                 href="#"
                 className="block px-4 py-2 hover:text-sky-900 transition-all duration-300"
               >
-                Twitter
+                About
               </Link>
             </li>
             <li>
@@ -31,7 +31,7 @@ const Navbar = () => {
                 href="#"
                 className="block px-4 py-2 hover:text-sky-900 transition-all duration-300"
               >
-                Qiita
+                Work
               </Link>
             </li>
           </ul>
